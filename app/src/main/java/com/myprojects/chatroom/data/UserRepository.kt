8977330504c050edf597d52e7e08c8b064ddd67c
Saifuddin <adenwala32@gmail.com>
2024-04-27 16:36:46 +0530

    user repository class created

---
+++ app/src/main/java/com/myprojects/chatroom/data/UserRepository.kt
@@ -0,0 +1,26 @@
+package com.myprojects.chatroom.data
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.myprojects.chatroom.data.Result.Error
+import kotlinx.coroutines.tasks.await
+
+class UserRepository(
+    private val auth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
+) {
+    suspend fun signUp(email: String, password: String, firstName: String, lastName: String) {
+        try {
+            auth.createUserWithEmailAndPassword(email, password)
+            val user = User(firstName, lastName, email)
+            saveUserToFirestore(user)
+            Result.Success(true)
+        }catch (e: Exception) {
+            Result.Error<Exception>(e)
+        }
+    }
+
+    private suspend fun saveUserToFirestore(user: User) {
+        firestore.collection("users").document(user.email).set(user).await()
+    }
+}
